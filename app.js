f606a0519ad176050dfbc6c8d31da3bfdc2f653b
Zin Wai Htet <kozinwaihtet@gmail.com>
2025-05-30 20:05:01 +0700

    filter dlc and remove null

--- app.js
+++ app.js
@@ -16,12 +16,19 @@ app.use(express.static('public'));
 app.use(compression());
 
 let gameAppId = [];
-let cachedGames = {};
 
 async function loadGameIDs() {
     try {
+        const cachedIDs = myCache.get(`all_game_ids`);
+        if (cachedIDs) {
+            gameAppId = cachedIDs;
+            console.log(`Loaded ${gameAppId.length} game IDs from cache`);
+            return;
+        }
+
         const response = await axios.get(steam_API);
         gameAppId = response.data.applist.apps.map(game => game.appid);
+        myCache.get(`all_game_ids`, gameAppId, 86400);
         console.log(`Loaded ${gameAppId.length} game IDs`);
     } catch (error) {
         console.log('Failed to load game IDs', error.message); 
@@ -29,16 +36,35 @@ async function loadGameIDs() {
 };
 
 async function gameDetails(appID) {
-    if (cachedGames[appID]) {
-        return cachedGames[appID];
+    const cachedGames = myCache.get(`game_${appID}`);
+    if (cachedGames) {
+        return cachedGames.type === 'dlc' ? null : cachedGames;
+    }
+
+    const cachedError = myCache.get(`error_${appID}`);
+    if (cachedError) {
+        return null;
     }
 
     try {
         const response = await axios.get(steam_API_details + appID);
-        const gameData = response.data[appID].data;
-        cachedGames[appID] = gameData;
+        const result = response.data[appID];
+        const gameData = result.data;
+
+        if (!result.success) return null;
+
+        if(!gameData || gameData.type === 'dlc') return null;
+
+        if (gameData && gameData.type === 'game') {
+            myCache.set(`game_${appID}`, gameData, 3600);
+
+            return gameData;
+        };
+
+        return null;
     } catch (error) {
         console.log(`Failed to load game ${appID}:`, error.message);
+        myCache.set(`error_${appID}`, true, 300);
         return null;
     }
 }
@@ -47,24 +73,34 @@ app.get('/', async (req, res) => {
     
     try { 
         const page = parseInt(req.query.page) || 1;
-        const gamePerPage = 20;
+        const gamePerPage = 30;
 
         const startIdx = (page - 1) * gamePerPage;
+        // console.log(`Start Index: ${startIdx}`);
+        
         const endIdx = startIdx + gamePerPage;
 
         const paginatedGames = gameAppId.slice(startIdx, endIdx);
 
+        // console.log(`paginated Games: ${paginatedGames}`);
+        
         const totalPages = Math.ceil(gameAppId.length /gamePerPage);
-    
+        // console.log(`Total Pages: ${totalPages}`);
+        
+        let attempts = 0;
+        const maxAttempts = 100;
+
         let games = [];
-
-        for (let i = 0; i < paginatedGames.length; i++){
-
-            const game = await gameDetails(gameAppId[i]);
-            if (game) {
-                games.push(game);
-            }
+        
+        for (let i = 0; i < paginatedGames.length && attempts < maxAttempts; i++) {
+            attempts++;
+            
+            const appID = paginatedGames[i];
+            const game = await gameDetails(appID);
+            if (game) games.push(game);
         };
+        
+        console.log(`Games On Page: ${games.length}`);
         
         res.render('home', { 
             games: games,
@@ -80,12 +116,18 @@ app.get('/', async (req, res) => {
 
 app.get('/detail', (req, res) => {
     res.render('detail')
-})
+});
 
-
-
-
-
+app.get('/dev-tool-cache', (req, res) => {
+    const stats = myCache.getStats();
+    res.json({
+        keys: myCache.keys(),
+        hits: stats.hits,
+        misses: stats.misses,
+        ksize: stats.ksize,
+        vsize: stats.vsize
+    });
+});
 
 loadGameIDs()
 
