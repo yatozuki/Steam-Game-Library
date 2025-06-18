9b2e2991cb1b2aefcdef4f6a79efb1b0805206ed
Zin Wai Htet <kozinwaihtet@gmail.com>
2025-06-18 22:40:13 +0700

    Detail page setup 80% complete

--- controllers/detailController.js
+++ controllers/detailController.js
@@ -2,16 +2,52 @@ import { detailData } from '../services/detailService.js';
 import { log } from '../utils/helpers.js';
 import chalk from 'chalk';
 
+export let gameDetail_dev = [];
+
 export async function getGameDetail(req, res) {
     const query = req.query.q?.trim().toLowerCase() || '';
     const appid = parseInt(req.params.id);
     log(appid);
 
-    // const game = await detailData(appid);
-    // log(game);
+    try {
+        const game = await detailData(appid);
 
+        if (!game.success) {
+            const error = `Request failed with status code 404.<br><strong>${appid}: <br>{ "success": false }</strong>`;
+            const message = `It seem like steam (appid = ${appid}) data can't be fetch via public API. (Might be NSFW or Violent game)`
+            res.render('error', {
+                query,
+                game,
+                caption: "404 Data Not Found!",
+                message,
+                error
+            });
 
-    res.render('detail', {
-        query
-    });
+        } else { 
+            const dataExist = gameDetail_dev.some(g => 
+                g.data.steam_appid === game.data.steam_appid
+            );
+
+            if (!dataExist) {
+                gameDetail_dev.push(game);
+            }
+
+            // log(game)
+
+            res.render('detail', {
+                query,
+                game: game.data
+            });
+
+        }
+    } catch (error) {
+        log(chalk.bgRed('Error', error.message));
+        res.render('error', {
+            query,
+            caption: "Something is wrong!",
+            message: 'Failed to load game detail',
+            error: error.message
+        });
+    }
+    
 }
--- services/detailService.js
+++ services/detailService.js
@@ -1,17 +1,26 @@
-import { steam_API_details, Scrape_MS, perPage } from '../config/constants.js';
-import { myCache } from '../config/cache.js';
-import { saveData, getGameData, updateGameData } from '../services/dataService.js';
+import { steam_API_details, Scrape_MS} from '../config/constants.js';
+import { myCache, cacheTime } from '../config/cache.js';
 import { log } from '../utils/helpers.js';
 import chalk from 'chalk';
 import axios from 'axios';
 
 export async function detailData(appid) {
-    let { detailInfo, detailSet } = getGameData();
-
+    const detailCache = myCache.get(appid);
+    if (detailCache) {
+        log(`Detail Information of "${detailCache.data.name}" is loaded`)
+        return detailCache
+    }
+    
     try {
         const response = await axios.get(steam_API_details + appid);
         const result = response.data[appid];
-        
+
+        if (result.success) {
+            myCache.set(appid, result, cacheTime);
+            return result;
+        } else {
+            return result;
+        }
     } catch (error) {
         log(`Error processing ${appid}:`, error.message)
     }
